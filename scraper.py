e8e6d3fa8af057c009f4e6ca04cc627f313c9f44
Abdallah72730 <ianomeit@gmail.com>
2025-12-21 20:59:14 -0700

    new scraper python file

---
+++ scraper.py
@@ -0,0 +1,39 @@
+import requests
+
+
+def construct_ebay_url(item_name: str) -> str:
+    searchquery = item_name.replace(" ", "+")
+
+    url = f"https://www.ebay.com/sch/i.html?_nkw={searchquery}&LH_Sold=1&LH_Complete=1"
+
+    return url
+
+
+def fetch_html_scraperapi (item_name: str) -> str:
+    target_url = construct_ebay_url(item_name)
+
+    encoded_target_url = requests.utils.quote(target_url, safe='')
+
+    api_url = f"http://api.scraperapi.com/?api_key={API_KEYS}&url={encoded_target_url}&render=true"
+
+    # params = {
+    #     "api-key": API_KEYS,
+    #     "url": target_url,
+    #     "render": "true",
+    #     "country_code": "ca"
+    # }
+
+    try:
+        response = requests.get(api_url, timeout=30)
+        response.raise_for_status()
+        return response.text
+    except requests.exceptions.RequestException as e:
+        print(f"Failed. {e} ")
+        return ""
+
+if __name__ == "__main__":
+    # print(construct_ebay_url("iphone 13"))
+
+    html = fetch_html_scraperapi("python programming book")
+
+    print(f"Got {len(html)} characters of Html")
